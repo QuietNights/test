f73273b997b1d151d2808089777538a9e7ae4d27
Caleb <Calebtagliaferri@hotmail.com>
2017-07-20 02:21:03 +0800

    Made player look lilgreenman

--- code/Player.java
+++ code/Player.java
@@ -2,12 +2,26 @@ package code;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Player extends GameObject {
 
+	File img = new File("Resources/lilgreenman.png");
+	
+	BufferedImage buffImg;
+	
 	public Player(int x, int y, ID id) {
 		super(x, y, id);
 		// TODO Auto-generated constructor stub
+		try {
+			buffImg = ImageIO.read(img);
+		} catch (IOException e) {
+			
+		}
 	}
 	
 	public void tick() {
@@ -15,8 +29,7 @@ public class Player extends GameObject {
 	}
 	
 	public void render(Graphics g) {
-		g.setColor(Color.white);
-		g.fillRect(50, 50, 50, 50);
+		g.drawImage(buffImg, 50, 50, null);
 	}
 	
 }

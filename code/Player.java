1eb2d2fd1c2893e231ee529f78f4500c5d7c6f30
BuildTools <unconfigured@null.spigotmc.org>
2017-07-20 02:23:02 +0800

    ad variables

--- code/Player.java
+++ code/Player.java
@@ -2,12 +2,46 @@ package code;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 
 public class Player extends GameObject {
+	
+	private int health;
+	private int maxHealth;
+	private int bullet;
+	private int maxBullet;
+	private boolean dead;
+	private boolean invuln;
+	private long invulnTime;
+	
+	private boolean firing;
+	private int bulletDamage;
+	
+	//private ArrayList<Bullet> bullets;
+	
+	private boolean jumping;
 
 	public Player(int x, int y, ID id) {
 		super(x, y, id);
 		// TODO Auto-generated constructor stub
+		
+		width = 7;
+		height = 12;
+		
+		moveSpeed = 0.5;
+		fallSpeed = 0.1;
+		maxFallSpeed = 4.0;
+		jumpStart = -4.5;
+		stopJumpSpeed = 0.3;
+		
+		facingRight = true;
+		
+		health = maxHealth = 5;
+		bullet = maxBullet = 10;
+		//bullets = new ArrayList<Bullet>();
 	}
 	
 	public void tick() {
@@ -15,7 +49,7 @@ public class Player extends GameObject {
 	}
 	
 	public void render(Graphics g) {
-		g.setColor(Color.white);
+		BufferedImage image = ImageIO.read();
 		g.fillRect(50, 50, 50, 50);
 	}
 	
